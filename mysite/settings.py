fb1d9a22b8e4027834e262ec635d1c698a585442
Sidharth Anand <sidharthanand67@gmail.com>
2022-04-25 18:29:50 +0530

    Added python-dotenv package

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG') == "True")
+DEBUG = os.environ.get('DEBUG') == "True"
 
 ALLOWED_HOSTS = []
 
